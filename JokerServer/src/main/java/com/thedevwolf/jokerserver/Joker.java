c4c440d49e9f36aebcdd80f350308afe8fd36bf2
Esmaeil Mohamed <thedevwolf@gmail.com>
2018-04-19 22:50:46 +0200

    Create JokerProvider "Java Library Module" to provide jokes to app Create JokerViewer "Android Library Module" to display jokes

---
+++ JokerServer/src/main/java/com/thedevwolf/jokerserver/Joker.java
@@ -0,0 +1,28 @@
+package com.thedevwolf.jokerserver;
+
+import java.util.Random;
+
+public class Joker {
+   private static String[] jokes={"Q: What is Easter Bunny’s favorite kind of music?A: Hip-hop, of course!",
+            "Q: Why are bunnies the luckiest animals?A: Because they each have four rabbits feet.",
+            "Q: What the Easter Bunny’s favorite dance move?A: The bunny hop.",
+            "Q: What day does an Easter egg hate the most?A: Fry-day",
+            "Q: What music does the Easter bunny like?A: Hip-hop",
+            "Q: Why does Peter Cottontail go hopping down the bunny trail?A: Because he's too young to drive.",
+            "Q: What did the Easter bunny say to the carrot?A: It's been nice gnawing you",
+            "Q: Why does the Easter Bunny hide?nA: Because he’s a little chicken.",
+            "Q: What sport are the eggs good at?A: Running",
+            "Q: What do you call an egg from outer space?A: An “Egg-stra terrestial”",
+            "Q: What happened when the Easter Bunny met the rabbit of his dreams?A: They lived hoppily ever after",
+            "Q: Why was the Easter bunny upset?A: He was having a bad hare day",};
+
+    Random random=new Random();
+
+
+
+    public String tellJoke(){
+        //12 is the maximum and the 1 is our minimum
+        int  n = random.nextInt(12) + 1;
+        return jokes[n];
+    }
+}
